0aa949bec998dc89791796db2c63da6a4945c3af
Pedro Illaisaca <pillaisaca@est.ups.edu.ec>
2019-07-26 15:21:56 -0500

    Optimizacion

--- src/Client.java
+++ src/Client.java
@@ -1,61 +1,140 @@
-import java.io.*;
-import java.net.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
 
-public class Client {
-	//COMPROBANO NUEVA RAMA PEDRO 
-    private final static int SOCKET_PORT = 2205;      // you may change this
-    private final static String SERVER = "localhost";  // localhost
-    private static String pathToSave = "/home/Leo/Documents/Recive/File";
-    private final static int FILE_SIZE = 6022386;
-    /*
-     * 
-     * ella no te ama
-     * 
-     * 
-     */
-    public static void main(String args[])throws IOException{
-        int bytesRead;
-        int current = 0;
-        FileOutputStream fos = null;
-        BufferedOutputStream bos = null;
-        Socket sock = null;
-        try {
-            sock = new Socket(SERVER, SOCKET_PORT);
-            System.out.println("Connecting...");
-            byte [] mybytearray  = new byte [FILE_SIZE];
-            InputStream is = sock.getInputStream();
-            fos = new FileOutputStream(pathToSave);
-            bos = new BufferedOutputStream(fos);
-            bytesRead = is.read(mybytearray,0,mybytearray.length);
 
-            current = bytesRead;
-            do {
-                bytesRead =
-                        is.read(mybytearray, current, (mybytearray.length-current));
-                if(bytesRead >= 0) current += bytesRead;
-            } while(bytesRead > -1);
 
-            bos.write(mybytearray, 0 , current);
-            bos.flush();
-            System.out.println("File " + pathToSave
-                    + " downloaded (" + current + " bytes read)");
-        }
-        finally {
-            if (fos != null) fos.close();
-            if (bos != null) bos.close();
-            if (sock != null) sock.close();
-        }
-    }
 
-    public void createFile(String path){
-        try{
-            File filesito = new File(path);
-            filesito.createNewFile();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        pathToSave = path;
-    }
+public class Client
+  extends JFrame
+{
+  private JTextField txtName;
+  private JTextField txtPath;
+  private ActionListener taskPerformer;
+  private Timer timer;
+  private boolean flag;
+  private int counter;
+  private static final int SOCKET_PORT = 2205;
+  private static final String SERVER = "10.10.10.10";
+  private static String pathToSave = ""; private static final int FILE_SIZE = 9999999;
+  
+  public Client() {
+    this.counter = 0;
+    init();
+  }
+  
+  public void init() {
+    setTitle("Cliente");
+    setDefaultCloseOperation(3);
+    setSize(299, 100);
+    setVisible(true);
+    setLocationRelativeTo(null);
+    setLayout(new BorderLayout());
+    components();
+    setSize(350, 100);
+  }
+  
+  public void components() {
+    JPanel pnl1 = new JPanel(new GridLayout(2, 2));
+    JPanel pnl2 = new JPanel(new FlowLayout());
+    
+    JButton btnStart = new JButton("Iniciar Recepcion");
+    btnStart.addActionListener(e -> {
+          try {
+            recibir();
+          } catch (IOException e1) {
+            
+            e1.printStackTrace();
+          } 
+        });
+    JButton btnClean = new JButton("Limpiar");
+    btnClean.addActionListener(e -> clean());
+    
+    JLabel lblName = new JLabel("Nombre del archivo:");
+    JLabel lblPath = new JLabel("Direccion de recepcion:");
+    
+    this.txtName = new JTextField(30);
+    this.txtPath = new JTextField(30);
+    
+    pnl1.add(lblName, new GridLayout(1, 1));
+    pnl1.add(this.txtName, new GridLayout(1, 2));
+    pnl1.add(lblPath, new GridLayout(2, 1));
+    pnl1.add(this.txtPath, new GridLayout(2, 2));
+    
+    pnl2.add(btnStart);
+    pnl2.add(btnClean);
+    
+    add(pnl1, "Center");
+    add(pnl2, "South");
+  }
+  
+  public void clean() {
+    this.txtName.setText("");
+    this.txtPath.setText("");
+    this.txtName.requestFocus();
+  }
+  
+  public void recibir() throws IOException {
+    sendName(this.txtName.getText());
+    
+    int current = 0;
+    FileOutputStream fos = null;
+    BufferedOutputStream bos = null;
+    Socket sock = null;
+    try {
+      try {
+        sock = new Socket("10.10.10.10", 2205);
+      } catch (Exception e) {
+        System.out.println("ERROR ERROR ERROR ERROR");
+      } 
+      System.out.println("Connecting...");
+      byte[] mybytearray = new byte[9999999];
+      InputStream is = sock.getInputStream();
+      fos = new FileOutputStream(pathToSave);
+      bos = new BufferedOutputStream(fos);
+      int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+      current = bytesRead;
+      do {
+        bytesRead = is.read(mybytearray, current, mybytearray.length - current);
+        if (bytesRead < 0) continue;  current += bytesRead;
+      } while (bytesRead > -1);
+      bos.write(mybytearray, 0, current);
+      bos.flush();
+      System.out.println("File " + pathToSave + " downloaded (" + current + " bytes read)");
+    } finally {
+      
+      if (fos != null) fos.close(); 
+      if (bos != null) bos.close(); 
+      if (sock != null) sock.close(); 
+    } 
+  }
+  
+  public void sendName(String file_name) {
+    System.out.println(file_name);
+    String leido = this.txtPath.getText();
+    File file = new File(String.valueOf(leido) + file_name);
+    try {
+      file.createNewFile();
+      pathToSave = String.valueOf(leido) + file_name;
+    } catch (IOException e) {
+      e.printStackTrace();
+    } 
+  }
+
+  
+  public static void main(String[] args) { Client client = new Client(); }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,118 +1,104 @@
-import javax.swing.*;
-import java.awt.*;
-import java.io.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Server extends JFrame {
+  public JTextField txtAdd;
+  private JButton btnSend;
+  private JLabel lbl1;
+  private File file;
+  private ServerSocket serverSocket;
+  
+  public Server() { init(); }
+  private Socket socket; private BufferedInputStream bufferedInputStream; private FileInputStream fileInputStream; private OutputStream outputStream; public static final int SOCKET_PORT = 2205;
+  
+  public void init() {
+    setTitle("Server");
+    setSize(399, 399);
+    setVisible(true);
+    setDefaultCloseOperation(3);
+    components();
+    setSize(400, 400);
+  }
+  
+  public void components() {
+    this.lbl1 = new JLabel("Please enter the file address");
+    this.txtAdd = new JTextField(20);
+    this.btnSend = new JButton("Send File");
+    
+    JPanel pnl1 = new JPanel(new BorderLayout());
+    JPanel pnl2 = new JPanel(new FlowLayout());
+    
+    pnl1.add(this.lbl1, "North");
+    pnl2.add(this.btnSend);
+    pnl2.add(this.txtAdd);
+    pnl1.add(pnl2, "South");
+    
+    add(pnl1);
+    
+    try {
+      this.serverSocket = new ServerSocket(2205);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } 
 
-    public JTextField txtAdd;
-    private JButton btnSend;
-    private JLabel lbl1;
-
-    private File file;
-    private ServerSocket serverSocket;
-    private Socket socket;
-    private BufferedInputStream bufferedInputStream;
-    private FileInputStream fileInputStream;
-    private OutputStream outputStream;
-
-    private int inttry = 0;
-
-    public final static int SOCKET_PORT = 2205;
-
-    public JProgressBar progressBar;
-
-    public Server(){
-        init();
-    }
-
-    public void init(){
-        this.setTitle("Server");
-        this.setSize(399,399);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(3);
-        components();
-    }
-
-    public void components(){
-        lbl1 = new JLabel("Please enter the file address");
-        txtAdd = new JTextField(20);
-        btnSend = new JButton("Send File");
-        progressBar = new JProgressBar();
-
-        JPanel pnl1 = new JPanel(new BorderLayout());
-        JPanel pnl2 = new JPanel(new FlowLayout());
-
-        pnl1.add(lbl1, BorderLayout.NORTH);
-        pnl1.add(progressBar, BorderLayout.CENTER);
-        pnl2.add(btnSend);
-        pnl2.add(txtAdd);
-        pnl1.add(pnl2, BorderLayout.SOUTH);
-
-        add(pnl1);
-
-        try {
-            serverSocket = new ServerSocket(SOCKET_PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        btnSend.addActionListener(e -> {
-            try {
-                sendFile(txtAdd.getText());
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+    
+    this.btnSend.addActionListener(e -> {
+          try {
+            sendFile(this.txtAdd.getText());
+          } catch (Exception ex) {
+            ex.printStackTrace();
+          } 
         });
+  }
 
+  
+  public void sendFile(String file2send) throws Exception {
+    this.fileInputStream = null;
+    this.bufferedInputStream = null;
+    this.outputStream = null;
+    this.socket = null;
+    
+    try {
+      System.out.println("Waiting...");
+      try {
+        this.socket = this.serverSocket.accept();
+        this.file = new File(file2send);
+        byte[] bytes = new byte[(int)this.file.length()];
+        this.fileInputStream = new FileInputStream(this.file);
+        this.bufferedInputStream = new BufferedInputStream(this.fileInputStream);
+        this.bufferedInputStream.read(bytes, 0, bytes.length);
+        this.outputStream = this.socket.getOutputStream();
+        this.outputStream.write(bytes, 0, bytes.length);
+        this.outputStream.flush();
+        System.out.println("Done");
+      } finally {}
+      if (this.bufferedInputStream != null)
+        this.bufferedInputStream.close(); 
+      if (this.outputStream != null)
+        this.outputStream.close(); 
+      if (this.socket != null) {
+        this.socket.close();
+      }
     }
-
-	public void sendFile(String file2send) throws Exception{
-        inttry += 1;
-
-        JOptionPane.showMessageDialog(this, "Try number: "+ inttry);
-
-        fileInputStream = null;
-        bufferedInputStream = null;
-        outputStream = null;
-        //serverSocket = null;
-        socket = null;
-
-        try{
-            while (true){
-                System.out.println("Waiting...");
-                try{
-                    socket = serverSocket.accept();
-                    file = new File(file2send);
-                    byte [] bytes = new byte[(int)file.length()];
-                    fileInputStream = new FileInputStream(file);
-                    bufferedInputStream = new BufferedInputStream(fileInputStream);
-                    bufferedInputStream.read(bytes, 0, bytes.length);
-                    outputStream = socket.getOutputStream();
-                    outputStream.write(bytes, 0, bytes.length);
-                    outputStream.flush();
-                    System.out.println("Done");
-
-                }finally {
-                    if(bufferedInputStream != null)
-                        bufferedInputStream.close();
-                    if(outputStream != null)
-                        outputStream.close();
-                    if(socket != null)
-                        socket.close();
-                    break;
-                }
-            }
-        }finally {
-            if(serverSocket != null)
-                serverSocket.close();
-        }
-    }
-
-    public static void main(String args[]){
-        Server server = new Server();
-    }
+    finally {
+      
+      if (this.serverSocket != null) {
+        this.serverSocket.close();
+      }
+    } 
+  }
+  
+  public static void main(String[] args) { Server server = new Server(); }
 }
-
